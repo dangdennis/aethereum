8cb6a2f73bee990100394041039bdc2980663565
Dennis Dang <dangggdennis@gmail.com>
2022-10-17 00:33:14 -0400

    feat: private to public key api
    commit 131ecb1dbf19b164dce49e138423d5b46c31b5ed
    Author: Dennis Dang <dangggdennis@gmail.com>
    Date:   Mon Oct 17 00:31:45 2022 -0400
    
        prefix pub key
    
    commit 87bc2e6458b1234f0b90bd4da43b743a2a90b0b1
    Author: Dennis Dang <dangggdennis@gmail.com>
    Date:   Mon Oct 17 00:27:19 2022 -0400
    
        api to derive public key from private

--- src/address.ml
+++ src/address.ml
@@ -1,10 +1,9 @@
 module Secp256k1 = Libsecp256k1.External
 
 let ctx = Secp256k1.Context.create ~sign:true ~verify:true ()
-
 let buffer_of_hex s = Cstruct.to_bigarray (Hex.to_cstruct (`Hex s))
 
-let hex_of_buffer (b) =
+let hex_of_buffer b =
   let t = Cstruct.of_bigarray b in
   let h = Hex.of_cstruct t in
   Hex.show h
@@ -35,6 +34,18 @@ let compute_address pk =
 
   String.sub keccaked_pk 24 (String.length keccaked_pk - 24) |> prefix_with_hex
 
+let public_key_of priv_key =
+  let seckey = buffer_of_hex priv_key in
+  let seckey = Secp256k1.Key.read_sk_exn ctx seckey in
+  let pubkey = Secp256k1.Key.neuterize_exn ctx seckey in
+  (* let buf_pk_uncomp = Cstruct.create 65 in *)
+  (* let nb_written =
+       Secp256k1.Key.write ~compress:false ctx buf_pk_uncomp.buffer pubkey
+     in *)
+  (* print_int nb_written; *)
+  let pubkey_serialized = Secp256k1.Key.to_bytes ~compress:false ctx pubkey in
+  hex_of_buffer pubkey_serialized |> prefix_with_hex
+
 let%expect_test _ =
   let pub_key =
     "02f7737e45b43dce88b03a0efba377b733dc21a65559fda9f015c3532337877619"
@@ -43,4 +54,11 @@ let%expect_test _ =
   print_string address;
   [%expect {| 0x1b64c49b4b6c195dc2475c41e3dd01cc5ba459c9 |}]
 
-
+let%expect_test "private key to public key" =
+  let priv_key =
+    "38f5740252df03191dd384295815af9b6607bc6b550601632971d658dbb2d583"
+  in
+  let pub_key = public_key_of priv_key in
+  print_string pub_key;
+  [%expect
+    {| 0x0404133ce7c511360d0967a2f9ed6860049d813a3df08cdf6a3ecad8d793af006f77ba8fe70fb191ef1fca960ea33794bc49f729464fd1f6520860f15c8eecb861 |}]
