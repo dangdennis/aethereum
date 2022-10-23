4e4f86f51536af5d966f3aa5b8e80dffb2c40978
Dennis Dang <dangggdennis@gmail.com>
2022-10-22 20:37:53 -0400

    use fetch-eio

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,5 @@
 let () =
   let url = "http://localhost:8545" in
-  Eio_main.run @@ fun env ->
-  Eio.Switch.run @@ fun sw ->
-  let s =
-    Aethereum.Jsonrpc.Web3.sha3 ~env ~sw ~url
-      "0xf7737e45b43dce88b03a0efba377b733dc21a65559fda9f015c35323378776192753ecba14089a0325b7788fed7265568f943fe3f55f738a79224f4515d13d4c"
-  in
-  print_endline s
+  Aethereum.Jsonrpc.Web3.sha3 ~url
+    "0xf7737e45b43dce88b03a0efba377b733dc21a65559fda9f015c35323378776192753ecba14089a0325b7788fed7265568f943fe3f55f738a79224f4515d13d4c"
+  |> print_endline
--- src/web3.ml
+++ src/web3.ml
@@ -1,24 +1,15 @@
-open Cohttp_eio
-
-let sha3 ~env ~sw ~url hashable =
-  let uri = Uri.of_string url in
-  let conn = Request.make_conn ~env ~sw ~uri in
-  let host = (Uri.host_with_default ~default:"localhost" uri, Uri.port uri) in
-  let headers = Http.Header.of_list [ ("content-type", "application/json") ] in
+let sha3 ~url hashable =
   let req_body =
     {
       Request.jsonrpc = "2.0";
       _method = "web3_sha3";
       params = [ hashable ];
-      id = 64;
       (* what should id be *)
+      id = 64;
     }
   in
-  let body =
-    Body.Fixed (Request.jsonaf_of_request req_body |> Jsonaf.to_string)
-  in
-  let res = Client.post ~conn ~headers ~body host "/" in
-  Client.read_fixed res
+  let body = Request.jsonaf_of_request req_body |> Jsonaf.to_string in
+  Fetch_eio.post ~body url
 
 (* let%expect_test "sha3" =
    let () = Eio_main.run @@ fun env -> Web3.sha3 env "" in
