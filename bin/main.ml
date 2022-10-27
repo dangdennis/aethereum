444b73d36f86ed340b304b816924254d384771fd
Dennis Dang <dangggdennis@gmail.com>
2022-10-27 12:50:59 -0400

    feat: wrap web3 and net apis (#3)

--- bin/main.ml
+++ bin/main.ml
@@ -8,4 +8,7 @@ let () =
     Aethereum.Jsonrpc.Web3.sha3 t
       "0xf7737e45b43dce88b03a0efba377b733dc21a65559fda9f015c35323378776192753ecba14089a0325b7788fed7265568f943fe3f55f738a79224f4515d13d4c"
   in
-  print_endline resp
+  print_endline resp;
+  Aethereum.Jsonrpc.Web3.client_version t |> print_endline;
+  Aethereum.Jsonrpc.Net.version t |> print_endline;
+  Aethereum.Jsonrpc.Net.listening t |> print_endline;
--- src/aethereum/net.ml
+++ src/aethereum/net.ml
@@ -1,21 +1,20 @@
-let sha3 t hashable =
+let version t =
   let req =
     {
       Request.jsonrpc = "2.0";
-      _method = "web3_sha3";
-      params = [ hashable ];
-      (* what should id be *)
+      _method = "net_version";
+      params = [ ];
       id = 64;
     }
   in
   let body = Request.jsonaf_of_t req |> Jsonaf.to_string in
   Fetch.post ~body t "/"
 
-let client_version t =
+let listening t =
   let req =
     {
       Request.jsonrpc = "2.0";
-      _method = "web3_clientVersion";
+      _method = "net_listening";
       params = [];
       id = 1;
     }
